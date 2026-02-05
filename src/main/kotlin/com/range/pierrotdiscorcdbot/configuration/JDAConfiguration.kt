eb7434f98b6ac8d647935bb07d510989687623e8
range79 <darkrange006@gmail.com>
2026-02-05 21:19:52 +0400

    ollama dockerfile added

--- src/main/kotlin/com/range/pierrotdiscorcdbot/configuration/JDAConfiguration.kt
+++ src/main/kotlin/com/range/pierrotdiscorcdbot/configuration/JDAConfiguration.kt
@@ -1,12 +1,18 @@
 package com.range.pierrotdiscorcdbot.configuration
 
 import com.range.pierrotdiscorcdbot.properties.BotProperties
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.JDABuilder
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class JDAConfiguration(
     private val botProperties: BotProperties
 ) {
-
-
+    @Bean
+    fun config(): JDA {
+        val jda = JDABuilder.createDefault(botProperties.token).build()
+        return jda
+    }
 }
