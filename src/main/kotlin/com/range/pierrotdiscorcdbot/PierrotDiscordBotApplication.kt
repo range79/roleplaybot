ee9458b4af26fcba8360a26a8e1b5813765182f9
range79 <darkrange006@gmail.com>
2026-02-05 16:20:30 +0400

    typo fixed

--- src/main/kotlin/com/range/pierrotdiscorcdbot/PierrotDiscordBotApplication.kt
+++ src/main/kotlin/com/range/pierrotdiscorcdbot/PierrotDiscordBotApplication.kt
@@ -1,14 +1,12 @@
 package com.range.pierrotdiscorcdbot
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 @ConfigurationPropertiesScan
 @SpringBootApplication
-class PierrotDiscorcdBotApplication
+class PierrotDiscordBotApplication
 
 fun main(args: Array<String>) {
-    runApplication<PierrotDiscorcdBotApplication>(*args)
+    runApplication<PierrotDiscordBotApplication>(*args)
 }
